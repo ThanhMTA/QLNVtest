7337be92dc84a05951920840800fdf31896446cb
ThanhMTA <trinhthithanh22032001@gmail.com>
2022-04-19 22:35:14 +0700

    kiet

---
+++ QLNV/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace QLNV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnwatch = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtmanv = new System.Windows.Forms.TextBox();
+            this.txtmada = new System.Windows.Forms.TextBox();
+            this.txttg = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnsua = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(2, 40);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(5);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(328, 313);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // btnwatch
+            // 
+            this.btnwatch.Location = new System.Drawing.Point(2, 373);
+            this.btnwatch.Margin = new System.Windows.Forms.Padding(5);
+            this.btnwatch.Name = "btnwatch";
+            this.btnwatch.Size = new System.Drawing.Size(328, 35);
+            this.btnwatch.TabIndex = 1;
+            this.btnwatch.Text = "xem";
+            this.btnwatch.UseVisualStyleBackColor = true;
+            this.btnwatch.Click += new System.EventHandler(this.btnwatch_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(340, 321);
+            this.button1.Margin = new System.Windows.Forms.Padding(5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 35);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "them";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(454, 40);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "PHÂN CÔNG ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(350, 75);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "MANV:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(350, 127);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "MADA:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(350, 189);
+            this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "THỜI GIAN:";
+            // 
+            // txtmanv
+            // 
+            this.txtmanv.Location = new System.Drawing.Point(458, 75);
+            this.txtmanv.Name = "txtmanv";
+            this.txtmanv.Size = new System.Drawing.Size(180, 26);
+            this.txtmanv.TabIndex = 7;
+            // 
+            // txtmada
+            // 
+            this.txtmada.Location = new System.Drawing.Point(458, 124);
+            this.txtmada.Name = "txtmada";
+            this.txtmada.Size = new System.Drawing.Size(180, 26);
+            this.txtmada.TabIndex = 8;
+            // 
+            // txttg
+            // 
+            this.txttg.Location = new System.Drawing.Point(458, 183);
+            this.txttg.Name = "txttg";
+            this.txttg.Size = new System.Drawing.Size(180, 26);
+            this.txttg.TabIndex = 9;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.button2.Location = new System.Drawing.Point(445, 321);
+            this.button2.Margin = new System.Windows.Forms.Padding(5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 35);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "xoa";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnsua
+            // 
+            this.btnsua.BackColor = System.Drawing.Color.Yellow;
+            this.btnsua.Location = new System.Drawing.Point(557, 321);
+            this.btnsua.Margin = new System.Windows.Forms.Padding(5);
+            this.btnsua.Name = "btnsua";
+            this.btnsua.Size = new System.Drawing.Size(95, 35);
+            this.btnsua.TabIndex = 11;
+            this.btnsua.Text = "sua";
+            this.btnsua.UseVisualStyleBackColor = false;
+            this.btnsua.Click += new System.EventHandler(this.btnsua_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(340, 373);
+            this.button3.Margin = new System.Windows.Forms.Padding(5);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(312, 35);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "lưu";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Indigo;
+            this.panel1.Location = new System.Drawing.Point(2, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(665, 37);
+            this.panel1.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Blue;
+            this.ClientSize = new System.Drawing.Size(666, 415);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnsua);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txttg);
+            this.Controls.Add(this.txtmada);
+            this.Controls.Add(this.txtmanv);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnwatch);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnwatch;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtmanv;
+        private System.Windows.Forms.TextBox txtmada;
+        private System.Windows.Forms.TextBox txttg;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnsua;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ QLNV/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace QLNV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string flag = " ";
+
+        string sql = @"Data Source=TRINHTHITHANH;Initial Catalog=QLNV;Integrated Security=True";
+        private void btnwatch_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = getPhanCong().Tables[0];
+        }
+        public DataSet getPhanCong()
+        {
+            DataSet data = new DataSet();
+            
+            using (SqlConnection connection=new SqlConnection(sql))
+            {
+                connection.Open();
+                string query = " select * from PhanCong";
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                adapter.Fill(data);
+                connection.Close();
+            }
+
+            return data;
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+
+        {
+            flag = "them";
+            txtmada.Enabled = true;
+            txtmanv.Enabled = true;
+            txttg.Enabled = true;
+            
+            
+        }
+         public void add()
+
+        {
+           
+            using (SqlConnection connection = new SqlConnection(sql))
+            {
+                connection.Open();
+                
+                string query = "insert into PhanCong values('" + txtmanv.Text + "','" + txtmada.Text + "'," +float.Parse( txttg.Text)+ ")";
+                SqlCommand cmd = new SqlCommand(query,connection);
+                cmd.ExecuteNonQuery();
+             
+                connection.Close();
+
+
+            
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            flag = "xoa";
+            txtmada.Enabled = false;
+            txtmanv.Enabled = false;
+            txttg.Enabled = false;
+            if (MessageBox.Show("ban co that su muon xoa", " thong bao", MessageBoxButtons.YesNoCancel) == System.Windows.Forms.DialogResult.Yes)
+            {
+                xoa();
+                dataGridView1.DataSource = getPhanCong().Tables[0];
+            }
+        }
+       
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+           int  index = dataGridView1.CurrentCell.RowIndex;
+            DataTable dt = (DataTable)dataGridView1.DataSource;
+            if (dt.Rows.Count > 0)
+            {
+                txtmanv.Text = dataGridView1.Rows[index].Cells[0].Value.ToString();
+                txtmada.Text = dataGridView1.Rows[index].Cells[1].Value.ToString();
+                txttg.Text = dataGridView1.Rows[index].Cells[2].Value.ToString();
+            }
+        }
+
+        private void btnsua_Click(object sender, EventArgs e)
+        {
+
+
+
+            flag = "sua";
+            txtmada.Enabled = false;
+            txtmanv.Enabled = false;
+            txttg.Enabled = true;
+            
+
+
+
+        }
+        public void SUA()
+
+        {
+
+            using (SqlConnection connection = new SqlConnection(sql))
+            {
+                connection.Open();
+
+                string query = "UPDATE  PhanCong set  sogio=" + float.Parse(txttg.Text) + "where MaNV='"+txtmanv.Text+"' and MaDA='"+txtmada.Text+"'" ;
+                SqlCommand cmd = new SqlCommand(query, connection);
+                cmd.ExecuteNonQuery();
+
+                connection.Close();
+
+
+
+            }
+
+        }
+        public void xoa()
+        {
+            using (SqlConnection connection = new SqlConnection(sql))
+            {
+                connection.Open();
+
+                string query = "delete phancong  where MaNV='" + txtmanv.Text + "' and MaDA='" + txtmada.Text + "'";
+                SqlCommand cmd = new SqlCommand(query, connection);
+                cmd.ExecuteNonQuery();
+
+                connection.Close();
+
+
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (flag == "them")
+                add();
+            if (flag == "sua")
+                SUA();
+            
+            dataGridView1.DataSource = getPhanCong().Tables[0];
+        }
+    }
+}
